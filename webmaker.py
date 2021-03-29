014a1a3c64aa32756ad02e7f1ea265f2a76540d1
Undercover Stamp <byemc@outlook.com>
2021-03-29 12:17:14 +0100

    Add files via upload

---
+++ webmaker.py
@@ -0,0 +1,47 @@
+print('Loading "Website Maker" by ByeMC')
+print('Grabbing header file')
+
+headertitle = open("headertemp.html", 'rt') # opens 'headertemp.html' as a text file in read-only mode
+
+print('Header grabbed. Tech info:', headertitle)
+
+filename = input('Please type the file name for your page. ".html" will be added automatically. ') + '.html'
+
+try:
+    with open(filename, 'rt') as savecheck:
+        overwrite = input('There appears to be a file here. Overwrite? (Y/N): ')
+        if overwrite == 'Y':
+            print('Okay, whatever you say')
+            savecheck.close()
+        else:
+            print('Exiting...')
+            import sys
+            sys.exit()
+except FileNotFoundError:
+    print('FileNotFound, making the file')
+
+title = input ('Please insert the title of the webpage: ')
+
+result = headertitle.read() + title
+
+print('Added title, here is the techy stuff: \n', result)
+
+print('Grabbing Part 2 of the Header')
+
+header = open('headertemp2.html', 'rt')
+
+print('Header grabbed. Tech Info:', header)
+
+title2 = input('Please put the header for the webpage (using a <h1> tag, the big title at the top): ')
+
+body = input('Please type/paste the body text for the webpage (use <br> for a new line: \n')
+
+result = result + header.read() + '<h2>' + title2 + '</h2> \n <p>' + body + '</p> \n\n\n\n <p><small><small><small>Made with Website Maker by ByeMC</small></small></small></p>'
+
+print('Current result is: \n', result)
+
+resultfile = open(filename, "wt")
+
+resultfile.write(result)
+
+print('FILE WRITTEN')
