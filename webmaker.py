a7374d2591684a3c9f5ffac94fb0a357d0b881a3
Undercover Stamp <byemc@outlook.com>
2021-04-05 17:17:13 +0100

    updated to new system [v0.1]

--- webmaker.py
+++ webmaker.py
@@ -1,62 +1,87 @@
-print('Loading "Website Maker" by ByeMC')
-print('Website Maker v0.2-alpha.2')
-quit = 0
-while quit == 0:
-    print('''Website Maker CLI by ByeMC
-Please type the number next to the option you would like to open, or type "quit" to quit.''')
-    from time import sleep
-    import random
-    print('Loading options...')
-    sleep(random.random())
-    command = input('''1) Main program
-2) Documentaion
-3) Quit
+# these are bits of code that other programs can call
+def version():
+    asciiart = '''
+.__..........__.._........._ _.........__  __......._...............
+.\ \......../ / | |.......(_| |.......|  \/  |.....| |..............            
+..\ \../\../ __.| |__  ___ _| |_ ___..| \  / |.__ _| |._____ _ __ ..
+...\ \/  \/ / _ | '_ \/ __| | __/ _ \.| |\/| |/ _` | |/ / _ | '__|..
+....\  /\  |  __| |_) \__ | | ||  __/.| |..| | (_| |   |  __| |.....   
+.....\/..\/.\___|_.__/|___|_|\__\___|.|_|..|_|\__,_|_|\_\___|_|.....
+..................................................... by ByeMC; v1.0
 
-> ''')
+Contributers:
+ByeMC
 
-    if int(command) == 1:
-        filename = input('Please type the file name for your page. ".html" will be added automatically. ') + '.html'
-        title = input ('Please insert the title of the webpage: ')
-        title2 = input('Please put the header for the webpage (using a <h1> tag, the big title at the top): ')
-        body = input('Please type/paste the body text for the webpage (use <br> for a new line: \n')
-        try:
-            with open(filename, 'rt') as savecheck:
-                sleep(0.2)
-                overwrite = input('There appears to be a file here. Overwrite? (Y/N): ')
-                if overwrite == 'Y' or 'y' or 'yes':
-                    print('Okay, whatever you say')
-                    savecheck.close()
-                else:
-                    print('Exiting...')
-                    import sys
-                    sys.exit()
-        except FileNotFoundError:
-            print('FileNotFound, making the file')
-        print('Grabbing header file')
-        headertitle = open("headertemp.html", 'rt') # opens 'headertemp.html' as a text file in read-only mode
-        print('Header grabbed. Tech info:', headertitle)
-        print('Grabbing Part 2 of the Header')
-        header = open('headertemp2.html', 'rt')
-        print('Header grabbed. Tech Info:', header)
-        print('NEW! Grabbing footer...')
+<Did you do work on this program? Add your (user)name here!'''
+    print(asciiart)
+
+def fail(reason):
+    print('An error seems to have stopped this from working. Check https://byemc.github.io/docs/webmaker/ for more, or https://github.com/byemc/websitemaker/issues for support. Reason:', reason)
+    import sys
+    sys.exit()
+
+# this is what will run if you execute the file 
+asciiart = '''
+.__..........__.._........._ _.........__  __......._..BETA VERSION.
+.\ \......../ / | |.......(_| |.......|  \/  |.....| |..............            
+..\ \../\../ __.| |__  ___ _| |_ ___..| \  / |.__ _| |._____ _ __ ..
+...\ \/  \/ / _ | '_ \/ __| | __/ _ \.| |\/| |/ _` | |/ / _ | '__|..
+....\  /\  |  __| |_) \__ | | ||  __/.| |..| | (_| |   |  __| |.....   
+.....\/..\/.\___|_.__/|___|_|\__\___|.|_|..|_|\__,_|_|\_\___|_|.....
+..................................................... by ByeMC; v1.0
+'''
+print(asciiart)
+
+filename = input('Please type the file name for your page. ".html" will be added automatically. ') + '.html'
+title = input ('Please insert the title of the webpage: ')
+head = input('Please put the header for the webpage (using a <h1> tag, the big title at the top): ')
+body = input('''Please type/paste the body text for the webpage (use <br> for a new line: 
+''')
+print('Checking file')
+
+try: 
+    with open(filename, 'rt') as filecheck:
+        sleep(0.2)
+        overwrite = input('There appears to be a file here. Overwrite? (Y/N): ')
+        if overwrite == 'Y' or 'y' or 'yes':
+            print('Okay, whatever you say')
+            filecheck.close()
+        else:
+            print('Exiting...')
+            import sys
+            sys.exit()
+except FileNotFoundError:
+    print('FileNotFound, making the file')
+    print('Grabbing header file')
+
+try:
+    headertitle = open('headertemp.html', 'rt') # use the old method of making files
+    try: # old file path found, looking for next part
+        headertitle2 = open('headertemp2.html', 'rt')
+        print('Old header file type detected, using WebMaker v0.1-alpha - v0.2-alpha.2 generation')
+        print('Grabbing footer...')
         try:
             with open('footer.html', 'rt') as footcheck:
-                result = headertitle.read() + title + header.read() + '<h2>' + title2 + '''</h2>
-                <p>''' + body + '''</p>
+                result = headertitle.read() + title + headertitle2.read() + '<h2>' + head + '''</h2>
+<p>''' + body + '''</p>
 
-                <p><small><small>Made with <a href="https://byemc.itch.io/webmaker">Website Maker by ByeMC</a>
-                ''' + footcheck.read()
+<p><small><small>Made with <a href="https://byemc.itch.io/webmaker">Website Maker by ByeMC</a>
+''' + footcheck.read()
+
         except FileNotFoundError:
-                result = headertitle.read() + title + header.read() + '<h2>' + title2 + '''</h2>
-                <p>''' + body + '''</p>
+            result = headertitle.read() + title + headertitle2.read() + '<h2>' + head + '''</h2>
+<p>''' + body + '''</p>
     
-                <p><small><small>Made with <a href="https://byemc.itch.io/webmaker">Website Maker by ByeMC</a></small></small></p>
-                </body>
-                </html>'''
-        resultfile = open(filename, "wt")
-        resultfile.write(result)
-        print('FILE WRITTEN')
-        if int(command) == 2:
-            import webbrowser as web
-            web.open_new_tab('https://iamunsttv.github.io/webmaker/docs/')
+<p><small><small>Made with <a href="https://byemc.itch.io/webmaker">Website Maker by ByeMC</a></small></small></p>
+</body>
+</html>'''
 
+    except FileNotFoundError:
+        fail('Required files couldn\'t be found, please check the documentaion')
+except FileNotFoundError:
+    fail('New generation hasn\'t been made yet')
+    
+    
+resultfile = open(filename, "wt")
+resultfile.write(result)
+print('FILE WRITTEN')
